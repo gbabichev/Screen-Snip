56590577cf3069c154a85b97baba0bbc589f6b5c
George Babichev <gbabichev@xantrion.com>
2025-09-08 18:39:14 -0400

    Initial Commit

---
+++ Screen Snap/Screen_SnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Screen_SnapApp.swift
+//  Screen Snap
+//
+//  Created by George Babichev on 9/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Screen_SnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
